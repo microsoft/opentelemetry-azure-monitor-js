f8ff8330152752e56c283611412bd5cc88218755
Mark Wolff <mark.wolff@microsoft.com>
2020-04-13 14:59:37 -0400

    remove unneeded icacls

--- src/platform/nodejs/persist/fileSystemPersist.ts
+++ src/platform/nodejs/persist/fileSystemPersist.ts
@@ -1,63 +1,11 @@
 import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
-import * as childProcess from 'child_process';
 import { Logger } from '@opentelemetry/api';
 import { NoopLogger } from '@opentelemetry/core';
 import { PersistentStorage } from '../../../types';
 import { ExporterConfig, DEFAULT_EXPORTER_CONFIG } from '../../../config';
-
-function getShallowDirectorySize(directory: string, callback: (err: Error | null, size: number) => void) {
-  // Get the directory listing
-  fs.readdir(directory, (err, files) => {
-    if (err) {
-      callback(err, -1);
-      return;
-    }
-
-    if (files.length === 0) {
-      callback(null, 0);
-      return;
-    }
-
-    let error: Error | null = null;
-    let totalSize = 0;
-    let count = 0;
-
-    // Query all file sizes
-    files.forEach((file) => {
-      fs.stat(path.join(directory, file), (statErr, fileStats) => {
-        count += 1;
-
-        if (statErr) {
-          error = statErr;
-        } else if (fileStats.isFile()) {
-          totalSize += fileStats.size;
-        }
-
-        if (count === files.length) {
-          // Did we get an error?
-          if (error) {
-            callback(error, -1);
-          } else {
-            callback(error, totalSize);
-          }
-        }
-      });
-    });
-  });
-}
-
-function getACLArguments(directory: string, identity: string) {
-  return [
-    directory,
-    '/grant',
-    '*S-1-5-32-544:(OI)(CI)F', // Full permission for Administrators
-    '/grant',
-    `${identity}:(OI)(CI)F`, // Full permission for current user
-    '/inheritance:r',
-  ]; // Remove all inherited permissions
-}
+import { confirmDirExists, getShallowDirectorySize } from './fileSystemHelpers';
 
 export class FileSystemPersist implements PersistentStorage {
   static TEMPDIR_PREFIX = 'ot-azure-exporter-';
@@ -67,16 +15,6 @@ export class FileSystemPersist implements PersistentStorage {
   private readonly _options: ExporterConfig;
 
   private readonly _logger: Logger;
-
-  private static readonly USE_ICACLS = os.type() === 'Windows_NT';
-
-  private static ICACLS_PATH = `${process.env.systemdrive}/windows/system32/icacls.exe`;
-
-  private static POWERSHELL_PATH = `${process.env.systemdrive}/windows/system32/windowspowershell/v1.0/powershell.exe`;
-
-  private static ACLED_DIRECTORIES: { [id: string]: boolean } = {};
-
-  private static ACL_IDENTITY: string | null = null;
 
   constructor(options: Partial<ExporterConfig> = DEFAULT_EXPORTER_CONFIG) {
     this._options = { ...DEFAULT_EXPORTER_CONFIG, ...options };
@@ -151,7 +89,7 @@ export class FileSystemPersist implements PersistentStorage {
       FileSystemPersist.TEMPDIR_PREFIX + this._options.instrumentationKey!,
     );
 
-    this._confirmDirExists(directory, (error) => {
+    confirmDirExists(directory, (error) => {
       if (error) {
         this._logger.warn(`Error while checking/creating directory: `, error && error.message);
         cb(error);
@@ -185,104 +123,6 @@ export class FileSystemPersist implements PersistentStorage {
           cb(writeError, !writeError);
         });
       });
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  private _getACLIdentity(callback: (error: Error | null, identity?: string) => void) {
-    if (FileSystemPersist.ACL_IDENTITY) {
-      callback(null, FileSystemPersist.ACL_IDENTITY);
-      return;
-    }
-    const psProc = childProcess.spawn(
-      FileSystemPersist.POWERSHELL_PATH,
-      ['-Command', '[System.Security.Principal.WindowsIdentity]::GetCurrent().Name'],
-      <any>{
-        windowsHide: true,
-        stdio: ['ignore', 'pipe', 'pipe'], // Needed to prevent hanging on Win 7
-      },
-    );
-    let data = '';
-    psProc.stdout.on('data', (d: string) => {
-      data += d;
-    });
-    psProc.on('error', (e: Error) => callback(e));
-    psProc.on('close', (code: number) => {
-      FileSystemPersist.ACL_IDENTITY = data && data.trim();
-      return callback(
-        code === 0 ? null : new Error(`Getting ACL identity did not succeed (PS returned code ${code})`),
-        FileSystemPersist.ACL_IDENTITY,
-      );
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  private _runICACLS(args: string[], callback: (err: Error | null) => void) {
-    const aclProc = childProcess.spawn(FileSystemPersist.ICACLS_PATH, args, <any>{ windowsHide: true });
-    aclProc.on('error', (e) => callback(e));
-    aclProc.on('close', (code) => {
-      return callback(
-        code === 0
-          ? null
-          : new Error(`Setting ACL restrictions did not succeed (ICACLS returned code ${code})`),
-      );
-    });
-  }
-
-  private _applyACLRules(directory: string, callback: (err: Error | null) => void) {
-    if (!FileSystemPersist.USE_ICACLS) {
-      callback(null);
-      return;
-    }
-
-    // For performance, only run ACL rules if we haven't already during this session
-    if (FileSystemPersist.ACLED_DIRECTORIES[directory] === undefined) {
-      // Avoid multiple calls race condition by setting ACLED_DIRECTORIES to false for this directory immediately
-      // If batches are being failed faster than the processes spawned below return, some data won't be stored to disk
-      // This is better than the alternative of potentially infinitely spawned processes
-      FileSystemPersist.ACLED_DIRECTORIES[directory] = false;
-
-      // Restrict this directory to only current user and administrator access
-      this._getACLIdentity((err, identity) => {
-        if (err || !identity) {
-          FileSystemPersist.ACLED_DIRECTORIES[directory] = false; // false is used to cache failed (vs undefined which is "not yet tried")
-          callback(err);
-          return;
-        }
-
-        this._runICACLS(getACLArguments(directory, identity), (runIcaclsErr) => {
-          FileSystemPersist.ACLED_DIRECTORIES[directory] = !runIcaclsErr;
-          callback(runIcaclsErr);
-        });
-      });
-    } else {
-      callback(
-        FileSystemPersist.ACLED_DIRECTORIES[directory]
-          ? null
-          : new Error('Setting ACL restrictions did not succeed (cached result)'),
-      );
-    }
-  }
-
-  /**
-   * Computes the size (in bytes) of all files in a directory at the root level. Asynchronously.
-   */
-  private _confirmDirExists(directory: string, callback: (err: Error | null) => void): void {
-    fs.lstat(directory, (err, stats) => {
-      if (err && err.code === 'ENOENT') {
-        fs.mkdir(directory, (mkdirErr) => {
-          if (mkdirErr && mkdirErr.code !== 'EEXIST') {
-            // Handle race condition by ignoring EEXIST
-            callback(mkdirErr);
-          } else {
-            this._applyACLRules(directory, callback);
-          }
-        });
-      } else if (!err && stats.isDirectory()) {
-        this._applyACLRules(directory, callback);
-      } else {
-        callback(err || new Error('Path existed but was not a directory'));
-      }
     });
   }
 }
