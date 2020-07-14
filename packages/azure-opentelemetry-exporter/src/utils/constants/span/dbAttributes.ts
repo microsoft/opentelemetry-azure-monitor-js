f86645637a2e07fb7f173f9925fe3779ce575778
Mark Wolff <marwolff@microsoft.com>
2020-07-14 18:22:12 -0400

    chore(conventions): use semantic-conventions (#55)

--- packages/azure-opentelemetry-exporter/src/utils/constants/span/dbAttributes.ts
+++ packages/azure-opentelemetry-exporter/src/utils/constants/span/dbAttributes.ts
@@ -1,13 +1,13 @@
-// required by https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/data-semantic-conventions.md#databases-client-calls
-export const COMPONENT = 'component';
-export const DB_TYPE = 'db.type';
-export const DB_INSTANCE = 'db.instance';
-export const DB_STATEMENT = 'db.statement';
-export const PEER_ADDRESS = 'peer.address';
-export const PEER_HOSTNAME = 'peer.host';
+import * as conventions from '@opentelemetry/semantic-conventions';
+
+export const { DB_TYPE } = conventions.DatabaseAttribute;
+export const { DB_INSTANCE } = conventions.DatabaseAttribute;
+export const { DB_STATEMENT } = conventions.DatabaseAttribute;
+export const PEER_ADDRESS = conventions.GeneralAttribute.NET_PEER_ADDRESS;
+export const PEER_HOSTNAME = conventions.GeneralAttribute.NET_PEER_HOSTNAME;
 
 // Optionals
-export const PEER_PORT = 'peer.port';
-export const PEER_IPV4 = 'peer.ipv4';
-export const PEER_IPV6 = 'peer.ipv6';
-export const PEER_SERVICE = 'peer.service';
+export const PEER_PORT = conventions.GeneralAttribute.NET_PEER_PORT;
+export const PEER_IPV4 = conventions.GeneralAttribute.NET_PEER_IPV4;
+export const PEER_IPV6 = conventions.GeneralAttribute.NET_PEER_IPV6;
+export const PEER_SERVICE = conventions.GeneralAttribute.NET_PEER_SERVICE;
